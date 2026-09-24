454b9e82e6cc7bdca2844a50875ba7dc2ddbea0b
atrenwick <77032171+atrenwick@users.noreply.github.com>
2026-09-24 16:43:05 +0200

    Change argument flags to use double dashes

--- cc_cnews/get_parquetfiles.py
+++ cc_cnews/get_parquetfiles.py
@@ -55,9 +55,9 @@ def download_files_for_year(year, repo_id, local_dir):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download specific parquet files from  a HF repo to a specified directory.")
-    parser.add_argument("-year",help="YEAR for folder in standard hierarchy ")
-    parser.add_argument("repo", help="path to HF repo")
-    parser.add_argument("-local_dir", help="absolute path to local directory where files will be downloaded")
+    parser.add_argument("--year",help="YEAR for folder in standard hierarchy ")
+    parser.add_argument("--repo", help="path to HF repo")
+    parser.add_argument("--local_dir", help="absolute path to local directory where files will be downloaded")
     args = parser.parse_args()
   
     year = str(args.year)
